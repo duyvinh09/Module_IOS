3165bd00881072537eeec5df5d08d0d858caf427
duyvinh09 <dinhduyvinh69@gmail.com>
2026-01-23 08:17:36 +0700

    test module locketgold

--- js/Locket_DuyVinh09.js
+++ js/Locket_DuyVinh09.js
@@ -9,28 +9,37 @@ var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
   obj = JSON.parse($response.body);
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 var duyvinh09 = {
-      is_sandbox: !1,
+      auto_resume_date: null,
+      display_name: "locket_1600_1y",
+      is_sandbox: true,
       ownership_type: "PURCHASED",
       billing_issues_detected_at: null,
+      management_url: "https://apps.apple.com/account/subscriptions",
       period_type: "normal",
-      expires_date: "2099-01-09T10:10:14Z",
+      price: {
+          "amount": 399000.0,
+          "currency": "VND"
+      },
+      expires_date: "9999-01-09T10:10:14Z",
       grace_period_expires_date: null,
+      refunded_at: null,
       unsubscribe_detected_at: null,
       original_purchase_date: "2005-01-09T10:10:15Z",
       purchase_date: "2005-01-09T10:10:14Z",
-      store: "app_store"
+      store: "app_store",
+      store_transaction_id: "2000001108724193",
   },
   locketGold = {
       grace_period_expires_date: null,
       purchase_date: "2005-01-09T10:10:14Z",
-      product_identifier: "com.duyvinh09.premium.yearly",
-      expires_date: "2099-01-09T10:10:14Z"
+      product_identifier: "locket_1600_1y",
+      expires_date: "9999-01-09T10:10:14Z"
   };
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["com.duyvinh09.premium.yearly"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
-} else obj.subscriber.subscriptions["com.duyvinh09.premium.yearly"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
+  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
+} else obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
 $done({
   body: JSON.stringify(obj)
 });
