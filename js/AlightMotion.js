ed895be4578e06599f0c9726e9e821b2645d23fe
duyvinh09 <dinhduyvinh69@gmail.com>
2026-06-14 17:51:40 +0700

    Update license details in AlightMotion.js to include additional benefits and correct product ID

--- js/AlightMotion.js
+++ js/AlightMotion.js
@@ -6,29 +6,30 @@ objc = {
         "msTime": 1704758400000,
         "accountCreatedMillis": null,
         "licenses": [
-        {
-            "benefits": [
-                "RemoveWatermark",
-                "MemberEffects",
-                "ProjectPackageSharing",
-                "FutureMemberFeatures",
-                "AdvancedEasing",
-                "CameraObjects",
-                "LayerParenting",
-                "CloudStorageLowTier"
-            ],
-            "type": "subscription",
-            "store": "apple_app_store",
-            "autoRenewing": true,
-            "orderNumber": "300001752007005",
-            "productId": "alightcreative.motion.1y_t60_1w",
-            "period": "1y",
-            "label": null,
-            "details": null,
-            "expires": 32662137600000,
-            "valid": true,
-            "linkStatus": "linked-current"
-        }
+            {
+                "benefits": [
+                    "RemoveWatermark",
+                    "MemberEffects",
+                    "ProjectPackageSharing",
+                    "FutureMemberFeatures",
+                    "AdvancedEasing",
+                    "CameraObjects",
+                    "LayerParenting",
+                    "CloudStorageLowTier",
+                    "CloudStorageHighTier"
+                ],
+                "type": "sub",
+                "store": "apple_app_store",
+                "autoRenewing": true,
+                "orderNumber": "300001752007005",
+                "productId": "alightcreative.motion.1y_t80_cloud_extra",
+                "period": "1y",
+                "label": null,
+                "details": null,
+                "expires": 32662137600000,
+                "valid": true,
+                "linkStatus": "linked-current"
+            }
         ],
         "warnings": []
     }
