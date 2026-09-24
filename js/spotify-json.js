8b007b7a7c599c9f9398c1c7de17eb7a0876e701
duyvinh09 <dinhduyvinh69@gmail.com>
2026-09-24 13:11:24 +0700

    update spotify module

--- js/spotify-json.js
+++ js/spotify-json.js
@@ -1,5 +1,8 @@
-console.log(`spotify-json-2023.06.13`);
+console.log(`spotify-json-2025.01.09`);
 let url = $request.url;
+if (url.includes('com:443')) {
+    url = url.replace(/com:443/, 'com');
+}
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
 } else {
